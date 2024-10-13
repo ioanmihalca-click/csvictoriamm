639ef002aa9ad232f097d471031c63692b81725c
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-10-13 17:20:44 +0300

    blog

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,6 +24,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->viteTheme('resources/css/app.css')
             ->id('admin')
             ->path('admin')
             ->login()
