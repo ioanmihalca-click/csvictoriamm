505d5cd2d3752c032a3bfa75abad970acec4ef74
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-10-13 17:27:34 +0300

    blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-
 export default {
-  presets: [preset],
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
-
   ],
   theme: {
     extend: {
