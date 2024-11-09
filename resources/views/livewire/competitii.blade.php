2315ff0efd8c6309ad02f4404002e47e511a6c8b
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-11-09 22:58:12 +0200

    competitie noua

--- resources/views/livewire/competitii.blade.php
+++ resources/views/livewire/competitii.blade.php
@@ -43,7 +43,7 @@
             Suntem mândri de performanța lui Sergiu Ioan Ciontoș, care a luptat cu înverșunare într-o piramidă de 8 sportivi în cadrul competiției "Centura Maramureșului", reușind să câștige două meciuri împotriva unor adversari foarte bine pregătiți.
         </p>
 
-        <a href="https://csvictoriamm.ro/blog/cs-victoria-maramures-straluceste-la-cupa-maramuresului-de-kickboxing" target="_blank" rel="noopener noreferrer" class="inline-flex items-center text-red-900 transition-colors duration-200 hover:text-red-700">
+        <a href="https://csvictoriamm.ro/blog/cs-victoria-maramures-a-participat-la-cupa-maramuresului-de-kickboxing" target="_blank" rel="noopener noreferrer" class="inline-flex items-center text-red-900 transition-colors duration-200 hover:text-red-700">
             Vezi mai multe detalii
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M5 12h14M12 5l7 7-7 7"/>
