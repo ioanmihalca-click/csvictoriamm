7c852824910db00b3670a04ba71e812934e31459
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2025-10-01 15:46:32 +0300

    update

--- resources/views/livewire/antrenamente.blade.php
+++ resources/views/livewire/antrenamente.blade.php
@@ -2,42 +2,61 @@
     <!-- Antrenamente Section -->
     <section class="py-16">
         <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <h2 class="mb-12 text-4xl font-extrabold text-center text-red-900 font-roboto-condensed">Antrenamente de Freestyle Kickboxing și Muay Thai</h2>
-            
+            <h2 class="mb-12 text-4xl font-extrabold text-center text-red-900 font-roboto-condensed">Antrenamente de
+                Freestyle Kickboxing și Muay Thai</h2>
+
             <div class="grid grid-cols-1 gap-12 lg:grid-cols-2">
                 <div class="space-y-6">
                     <div class="overflow-hidden bg-white shadow-md rounded-xl">
                         <div class="p-6">
-                            <h3 class="mb-4 text-2xl font-bold text-red-900">Pentru copii și adulți in Baia-Mare, Poienile de sub Munte si Petrova</h3>
-                            <p class="text-gray-600">Freestyle Kickboxing este o formă de arte marțiale care combină tehnici de box și lovituri cu piciorul. Acest stil de luptă este caracterizat de libertatea și creativitatea luptătorilor în a folosi diverse tehnici și mișcări pentru a-și învinge adversarii.</p>
+                            <h3 class="mb-4 text-2xl font-bold text-red-900">Pentru copii și adulți in Baia-Mare,
+                                Poienile de sub Munte si Petrova</h3>
+                            <p class="text-gray-600">Freestyle Kickboxing este o formă de arte marțiale care combină
+                                tehnici de box și lovituri cu piciorul. Acest stil de luptă este caracterizat de
+                                libertatea și creativitatea luptătorilor în a folosi diverse tehnici și mișcări pentru
+                                a-și învinge adversarii.</p>
                         </div>
                     </div>
                     <div class="overflow-hidden bg-white shadow-md rounded-xl">
                         <div class="p-6">
-                            <p class="mb-4 text-gray-600">Deși este similar cu alte stiluri de arte marțiale, precum kickboxingul și Muay Thai, Freestyle Kickboxing permite luptătorilor să-și dezvolte propriul stil și să experimenteze cu diverse tehnici și strategii. În acest sens, este considerat mai liber și mai flexibil decât alte stiluri tradiționale.</p>
-                            <p class="text-gray-600">Freestyle Kickboxing este practicat în scopuri recreative și de fitness, dar și în competiții sportive, inclusiv în evenimente profesioniste.</p>
+                            <p class="mb-4 text-gray-600">Deși este similar cu alte stiluri de arte marțiale, precum
+                                kickboxingul și Muay Thai, Freestyle Kickboxing permite luptătorilor să-și dezvolte
+                                propriul stil și să experimenteze cu diverse tehnici și strategii. În acest sens, este
+                                considerat mai liber și mai flexibil decât alte stiluri tradiționale.</p>
+                            <p class="text-gray-600">Freestyle Kickboxing este practicat în scopuri recreative și de
+                                fitness, dar și în competiții sportive, inclusiv în evenimente profesioniste.</p>
                         </div>
                     </div>
                 </div>
                 <div class="space-y-6">
                     <div class="overflow-hidden bg-white shadow-md rounded-xl">
-                        <img src="/assets/Kickboxing-Baia-Mare.webp" alt="Kickboxing in Baia Mare" class="w-full h-auto">
+                        <img src="/assets/Kickboxing-Baia-Mare.webp" alt="Kickboxing in Baia Mare"
+                            class="w-full h-auto">
                     </div>
                     <div class="overflow-hidden bg-white shadow-md rounded-xl">
                         <div class="p-6">
                             <h3 class="mb-4 text-2xl font-bold text-red-900">Orar Baia Mare:</h3>
                             <ul class="space-y-2 text-gray-600">
                                 <li class="flex items-center">
-                                    <svg class="w-5 h-5 mr-2 text-red-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                    De luni pana joi, ora 19:15 - Grupa adulti si juniori mari
+                                    <svg class="w-5 h-5 mr-2 text-red-900" fill="none" stroke="currentColor"
+                                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                    </svg>
+                                    Luni si Miercuri, ora 19:35 - Grupa adulti si juniori mari
                                 </li>
                                 <li class="flex items-center">
-                                    <svg class="w-5 h-5 mr-2 text-red-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                    Marti si joi, ora 18:00 - Grupa copii (6-12 ani)
+                                    <svg class="w-5 h-5 mr-2 text-red-900" fill="none" stroke="currentColor"
+                                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                    </svg>
+                                    Luni si Miercuri, ora 18:30 - Grupa copii (6-12 ani)
                                 </li>
                             </ul>
                             <p class="mt-4 text-gray-600">Vineri, ora 18:30 - Sala de sport Petrova - Grupa mixta</p>
-                            <p class="text-gray-600">Sambata, ora 13:00 - Sala de sport Poienile de sub Munte - Grupa mixta</p>
+                            <p class="text-gray-600">Sambata, ora 13:00 - Sala de sport Poienile de sub Munte - Grupa
+                                mixta</p>
                         </div>
                     </div>
                 </div>
@@ -46,22 +65,35 @@
             <div class="mt-12 overflow-hidden bg-white shadow-md rounded-xl">
                 <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="p-6">
-                        <img src="https://res.cloudinary.com/dadjiwkkf/image/upload/v1727107335/antrenamente_bcsnj1.jpg" alt="Kickboxing in Baia Mare" class="w-full h-auto rounded-lg">
+                        <img src="https://res.cloudinary.com/dadjiwkkf/image/upload/v1727107335/antrenamente_bcsnj1.jpg"
+                            alt="Kickboxing in Baia Mare" class="w-full h-auto rounded-lg">
                     </div>
                     <div class="p-6">
                         <h3 class="mb-4 text-2xl font-bold text-red-900">Antrenor: Ioan Mihalca</h3>
                         <p class="mb-2 text-gray-600">Certificări:</p>
                         <ul class="space-y-2 text-gray-600">
                             <li class="flex items-center">
-                                <svg class="w-5 h-5 mr-2 text-red-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                <svg class="w-5 h-5 mr-2 text-red-900" fill="none" stroke="currentColor"
+                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                </svg>
                                 Instructor Sportiv (Asociatia Internationala Jissen Do)
                             </li>
                             <li class="flex items-center">
-                                <svg class="w-5 h-5 mr-2 text-red-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                <svg class="w-5 h-5 mr-2 text-red-900" fill="none" stroke="currentColor"
+                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                </svg>
                                 Fitness & Functional Fitness (Fitness Education School)
                             </li>
                             <li class="flex items-center">
-                                <svg class="w-5 h-5 mr-2 text-red-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                <svg class="w-5 h-5 mr-2 text-red-900" fill="none" stroke="currentColor"
+                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                </svg>
                                 Freestyle Kickboxing Centura Neagra 2 Dan (Frfk)
                             </li>
                         </ul>
@@ -74,13 +106,20 @@
     <!-- Fitness Functional Section -->
     <section class="py-16 bg-white">
         <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <h2 class="mb-12 text-4xl font-extrabold text-center text-red-900 font-roboto-condensed">Fitness Funcțional in Baia Mare</h2>
-            
+            <h2 class="mb-12 text-4xl font-extrabold text-center text-red-900 font-roboto-condensed">Fitness Funcțional
+                in Baia Mare</h2>
+
             <div class="grid grid-cols-1 gap-12 lg:grid-cols-2">
                 <div class="space-y-6">
                     <div class="overflow-hidden bg-gray-100 shadow-md rounded-xl">
                         <div class="p-6">
-                            <p class="font-semibold text-gray-800">La Clubul Sportiv Victoria Maramureș, programele noastre de Fitness Funcțional sunt concepute pentru a satisface nevoile tuturor, de la amatori de sport si miscare până la atleți de elită. Oferim pregătire fizică adaptată și personalizată, care îmbină tehnici avansate și metodologii dovedite, pentru a ajuta atât amatorii să-și atingă obiectivele de sănătate și formă fizică, cât și sportivii de înaltă performanță să-și optimizeze capacitățile atletice și să exceleze în disciplinele lor specifice.</p>
+                            <p class="font-semibold text-gray-800">La Clubul Sportiv Victoria Maramureș, programele
+                                noastre de Fitness Funcțional sunt concepute pentru a satisface nevoile tuturor, de la
+                                amatori de sport si miscare până la atleți de elită. Oferim pregătire fizică adaptată și
+                                personalizată, care îmbină tehnici avansate și metodologii dovedite, pentru a ajuta atât
+                                amatorii să-și atingă obiectivele de sănătate și formă fizică, cât și sportivii de
+                                înaltă performanță să-și optimizeze capacitățile atletice și să exceleze în disciplinele
+                                lor specifice.</p>
                         </div>
                     </div>
                     <div class="overflow-hidden bg-gray-100 shadow-md rounded-xl">
@@ -88,19 +127,35 @@
                             <h3 class="mb-4 text-2xl font-bold text-red-900">Beneficii:</h3>
                             <ul class="space-y-2 text-gray-600">
                                 <li class="flex items-start">
-                                    <svg class="flex-shrink-0 w-6 h-6 mr-2 text-red-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                    <svg class="flex-shrink-0 w-6 h-6 mr-2 text-red-900" fill="none"
+                                        stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                    </svg>
                                     <span>Îmbunătățirea condiției fizice și mentale</span>
                                 </li>
                                 <li class="flex items-start">
-                                    <svg class="flex-shrink-0 w-6 h-6 mr-2 text-red-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                    <svg class="flex-shrink-0 w-6 h-6 mr-2 text-red-900" fill="none"
+                                        stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                    </svg>
                                     <span>Dezvoltarea forței, rezistenței, agilității și flexibilității</span>
                                 </li>
                                 <li class="flex items-start">
-                                    <svg class="flex-shrink-0 w-6 h-6 mr-2 text-red-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                    <svg class="flex-shrink-0 w-6 h-6 mr-2 text-red-900" fill="none"
+                                        stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                    </svg>
                                     <span>Accelerarea procesului de slăbire și tonifiere</span>
                                 </li>
                                 <li class="flex items-start">
-                                    <svg class="flex-shrink-0 w-6 h-6 mr-2 text-red-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                    <svg class="flex-shrink-0 w-6 h-6 mr-2 text-red-900" fill="none"
+                                        stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                    </svg>
                                     <span>Îmbunătățirea coordonării motorii și agilității</span>
                                 </li>
                             </ul>
@@ -111,14 +166,22 @@
                     <div class="overflow-hidden bg-gray-100 shadow-md rounded-xl">
                         <div class="p-6">
                             <h3 class="mb-4 text-2xl font-bold text-red-900">Despre Antrenamente</h3>
-                            <p class="mb-4 text-gray-600">Sesiunile noastre full-body, cu durata de 45-60 de minute, sunt concepute special pentru a lucra întregul corp și a accelera procesul de slăbire și tonifiere. Pe lângă dezvoltarea forței și rezistenței, vei îmbunătăți semnificativ coordonarea motorie și agilitatea.</p>
-                            <p class="text-gray-600">Dincolo de avantajele fizice, antrenamentele de Fitness Funcțional au un impact pozitiv asupra sănătății mentale și emoționale. Ele contribuie la reducerea stresului, creșterea stimei de sine și sporirea nivelului de energie.</p>
+                            <p class="mb-4 text-gray-600">Sesiunile noastre full-body, cu durata de 45-60 de minute,
+                                sunt concepute special pentru a lucra întregul corp și a accelera procesul de slăbire și
+                                tonifiere. Pe lângă dezvoltarea forței și rezistenței, vei îmbunătăți semnificativ
+                                coordonarea motorie și agilitatea.</p>
+                            <p class="text-gray-600">Dincolo de avantajele fizice, antrenamentele de Fitness Funcțional
+                                au un impact pozitiv asupra sănătății mentale și emoționale. Ele contribuie la reducerea
+                                stresului, creșterea stimei de sine și sporirea nivelului de energie.</p>
                         </div>
                     </div>
                     <div class="overflow-hidden bg-gray-100 shadow-md rounded-xl">
                         <div class="p-6">
                             <h3 class="mb-4 text-2xl font-bold text-red-900">Pentru Cine?</h3>
-                            <p class="text-gray-600">Indiferent de nivelul tău de formă fizică sau de obiectivele pe care le ai, clasele de grup și antrenamentele personale de Fitness Funcțional sunt potrivite pentru oricine dorește să-și îmbunătățească calitatea vieții și performanțele fizice.</p>
+                            <p class="text-gray-600">Indiferent de nivelul tău de formă fizică sau de obiectivele pe
+                                care le ai, clasele de grup și antrenamentele personale de Fitness Funcțional sunt
+                                potrivite pentru oricine dorește să-și îmbunătățească calitatea vieții și performanțele
+                                fizice.</p>
                         </div>
                     </div>
                 </div>
