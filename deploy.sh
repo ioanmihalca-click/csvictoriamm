fe061e43af59817ad43699cd3a8d2d682e1cba88
Click Studios Digital <ioanclickmihalca@gmail.com>
2026-05-10 12:06:44 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 cd ~/domains/csvictoriamm.ro/public_html
 
 git pull origin main
-/opt/alt/php84/usr/bin/php composer.phar install --no-dev --optimize-autoloader
-/opt/alt/php84/usr/bin/php artisan migrate --force
-/opt/alt/php84/usr/bin/php artisan config:cache
-/opt/alt/php84/usr/bin/php artisan route:cache
-/opt/alt/php84/usr/bin/php artisan view:cache
+composer install --no-dev --optimize-autoloader
+php artisan migrate --force
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
